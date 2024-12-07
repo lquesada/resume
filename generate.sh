e2f5ad61cecf9318a272084a1e4271aadf36b343
Luis Quesada <elezeta@gmail.com>
2024-12-07 22:25:49 +0100

    Add hidden link support

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ EOM
 
 alias pdf2htmlEX='docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex pdf2htmlEX'
 pdf2htmlEX --decompose-ligature 1 --font-size-multiplier 20 --zoom 2 --hdpi 600 --vdpi 600 --process-outline 0 resume.pdf
-cat resume.html | sed s/'<head>'/"<head>$ADD"/ > tmp.html
+cat resume.html | sed -e s/'<head>'/"<head>$"/ -e s@'href="https://0/local/'@'target="_blank" href="'@g > tmp.html
 mv -f tmp.html resume.html
 rm -f resume.aux resume.log resume.out
 mv -f resume.html index.html
