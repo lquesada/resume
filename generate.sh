9e8c2ab5f02a591e9f1e4dd8598773246a2b0a8f
Luis Quesada <elezeta@gmail.com>
2025-04-30 20:21:46 +0200

    Update

--- generate.sh
+++ generate.sh
@@ -4,10 +4,10 @@ ADD=$(cat << EOM | tr -d '\n'
 <link rel="shortcut icon" type="image\\/png" href="favicon.png"\\/>
 EOM
 )
-alias pdf2htmlEX='docker run -ti --rm -v `pwd`:/pdf bwits/pdf2htmlex pdf2htmlEX'
+alias pdf2htmlEX='~/bin/pdf2htmlEX.AppImage'
 
 xelatex resume.tex
-pdf2htmlEX --decompose-ligature 1 --font-size-multiplier 20 --zoom 2 --hdpi 600 --vdpi 600 --process-outline 0 resume.pdf
+pdf2htmlEX --decompose-ligature 1 --font-size-multiplier 20 --zoom 2 --process-outline 0 resume.pdf
 cat resume.html | sed -e s/'<head>'/"<head>$ADD"/g -e s@'href="https://0/local/'@'href="'@g > tmp.html
 mv -f tmp.html resume.html
 rm -f resume.aux resume.log resume.out
