99bded29838ef34e38be88739ec35bd5e8e3c0c5
Luis Quesada <elezeta@gmail.com>
2025-04-30 23:42:15 +0200

    Improve pic resolution

--- generate.sh
+++ generate.sh
@@ -7,7 +7,7 @@ EOM
 alias pdf2htmlEX='~/bin/pdf2htmlEX.AppImage'
 
 xelatex resume.tex
-pdf2htmlEX --decompose-ligature 1 --font-size-multiplier 20 --zoom 2 --process-outline 0 resume.pdf
+pdf2htmlEX --decompose-ligature 1 --font-size-multiplier 20 --zoom 2 --dpi 600 --process-outline 0 resume.pdf
 cat resume.html | sed -e s/'<head>'/"<head>$ADD"/g -e s@'href="https://0/local/'@'href="'@g > tmp.html
 mv -f tmp.html resume.html
 rm -f resume.aux resume.log resume.out
